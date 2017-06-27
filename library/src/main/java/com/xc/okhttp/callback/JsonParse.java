717542ce6662eaa0c3fe8e4ec8244d068e32cba1
xchengDroid <543174222@qq.com>
2017-06-27 09:26:58 +0800

    设置jsonBean也能返回String

--- library/src/main/java/com/xc/okhttp/callback/JsonParse.java
+++ library/src/main/java/com/xc/okhttp/callback/JsonParse.java
@@ -3,6 +3,7 @@ package com.xc.okhttp.callback;
 import android.support.annotation.NonNull;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.xc.okhttp.error.BaseError;
 import com.xc.okhttp.request.OkResponse;
 
@@ -15,11 +16,17 @@ import okhttp3.Response;
  */
 
 public class JsonParse<T> extends ResponseParse<T> {
+    @SuppressWarnings("unchecked")
     @NonNull
     @Override
     public OkResponse<T> parseNetworkResponse(OkCall<T> call, Response response, int id) throws IOException {
         if (response.isSuccessful()) {
-            T body = new Gson().fromJson(response.body().string(), call.getTypeToken().getType());
+            TypeToken<T> token = call.getTypeToken();
+            String str = response.body().string();
+            if (String.class.isAssignableFrom(token.getRawType())) {
+                return OkResponse.success((T) str);
+            }
+            T body = new Gson().fromJson(str, token.getType());
             return OkResponse.success(body);
         }
         return OkResponse.error(BaseError.getNotFoundError("not success response"));
