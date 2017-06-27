b24131de817e68223f2aa57172358480c6e39161
xchengDroid <543174222@qq.com>
2017-06-27 14:40:37 +0800

    设置OkConfig初始化线程同步，只能被初始化一次

--- library/src/main/java/com/xc/okhttp/EasyOkHttp.java
+++ library/src/main/java/com/xc/okhttp/EasyOkHttp.java
@@ -1,6 +1,8 @@
 package com.xc.okhttp;
 
 
+import android.util.Log;
+
 import com.xc.okhttp.request.GetRequest;
 import com.xc.okhttp.request.OkConfig;
 import com.xc.okhttp.request.PostStrRequest;
@@ -10,21 +12,28 @@ import okhttp3.Call;
 import okhttp3.OkHttpClient;
 
 /**
- * Created by zhy on 15/8/17.
+ * Created by chengxin on 17/6/22
  */
 public class EasyOkHttp {
 
     public static final long DEFAULT_MILLISECONDS = 10_000L;
     private static OkConfig sOkConfig;
+    public static final String TAG = EasyOkHttp.class.getSimpleName();
 
-    public static void init(OkConfig okConfig) {
-        OkExceptions.checkNotNull(okConfig, "okConfig==null");
+    public synchronized static void init(OkConfig okConfig) {
+        if (okConfig == null) {
+            OkExceptions.illegalArgument("OkConfig  can not be initialized with null");
+        }
+        if (sOkConfig != null) {
+            Log.e(TAG, "try to initialize OkConfig which had already been initialized before");
+            return;
+        }
         sOkConfig = okConfig;
     }
 
     public static OkConfig getOkConfig() {
         if (sOkConfig == null) {
-            OkExceptions.illegalState("OkConfig==null,please init EasyOkHttp before call this method !");
+            OkExceptions.illegalState("EasyOkHttp must be init with OkConfig before using");
         }
         return sOkConfig;
     }
