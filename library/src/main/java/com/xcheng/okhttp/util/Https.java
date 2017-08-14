be74984329827560177ba88687e91982e75a1df4
xchengDroid <543174222@qq.com>
2017-08-14 22:58:53 +0800

    review

--- library/src/main/java/com/xcheng/okhttp/util/Https.java
+++ library/src/main/java/com/xcheng/okhttp/util/Https.java
@@ -198,7 +198,7 @@ public class Https {
      * 则验证机制可以回调此接口的实现程序来确定是否应该允许此连接。策略可以是基于证书的或依赖于其他验证方案。
      * 当验证 URL 主机名使用的默认规则失败时使用这些回调。如果主机名是可接受的，则返回 true
      */
-    public static HostnameVerifier UnSafeHostnameVerifier = new HostnameVerifier() {
+    public final static HostnameVerifier UNSAFE_HOSTNAME_VERIFIER = new HostnameVerifier() {
         @Override
         public boolean verify(String hostname, SSLSession session) {
             return true;
