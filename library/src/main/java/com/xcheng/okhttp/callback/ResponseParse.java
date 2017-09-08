d921a4a8d2dc29136a0e124bc3e5efc42b0632dd
xchengDroid <543174222@qq.com>
2017-09-08 22:08:08 +0800

    review

--- library/src/main/java/com/xcheng/okhttp/callback/ResponseParse.java
+++ library/src/main/java/com/xcheng/okhttp/callback/ResponseParse.java
@@ -15,7 +15,9 @@ import okhttp3.Response;
  * http解析的工具 成功返回所需的对象
  */
 public interface ResponseParse<T> {
-
+    /**
+     * @throws NullPointerException 如果返回值为空
+     */
     @NonNull
     OkResponse<T> parseNetworkResponse(OkCall<T> okCall, Response response) throws IOException;
 
@@ -24,6 +26,7 @@ public interface ResponseParse<T> {
      *
      * @param e IO错误
      * @return EasyError  对应IOException的error信息
+     * @throws NullPointerException 如果返回值为空
      */
     @NonNull
     EasyError getError(IOException e);
