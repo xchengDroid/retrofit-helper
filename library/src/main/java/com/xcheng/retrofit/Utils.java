f97ec7441deac5d720adc79ad1234138be70efe6
chengxin <chengx@hlsteel.com>
2018-04-08 12:50:32 +0800

    添加工具类注释

--- library/src/main/java/com/xcheng/retrofit/Utils.java
+++ library/src/main/java/com/xcheng/retrofit/Utils.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 /**
  * 创建时间：2018/4/8
  * 编写人： chengxin
- * 功能描述：
+ * 功能描述：retrofit2工具类
  */
 public class Utils {
     static <T> T checkNotNull(@Nullable T object, String message) {
