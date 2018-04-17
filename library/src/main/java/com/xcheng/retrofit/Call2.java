beb6a678ae044454be43f305151092104b7235fa
chengxin <chengx@hlsteel.com>
2018-04-17 17:57:42 +0800

    修改注释

--- library/src/main/java/com/xcheng/retrofit/Call2.java
+++ library/src/main/java/com/xcheng/retrofit/Call2.java
@@ -1,20 +1,11 @@
 package com.xcheng.retrofit;
 
-import retrofit2.Callback;
-
 /**
  * 创建时间：2018/4/8
  * 编写人： chengxin
  * 功能描述：添加重载方法{@link Call2#enqueue(Object, Callback2)}方法
  */
 public interface Call2<T> extends retrofit2.Call<T> {
-
-    /**
-     * @throws UnsupportedOperationException if called this method
-     */
-    @Override
-    void enqueue(Callback<T> callback);
-
     /**
      * @param tag       请求的tag
      * @param callback2 请求的回调
