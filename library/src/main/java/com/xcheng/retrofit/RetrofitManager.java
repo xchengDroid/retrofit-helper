805945c74511e5c7c1814c2e9c9cff1e305e174b
chengxin <chengx@hlsteel.com>
2018-07-25 17:01:27 +0800

    添加集合保存保存多个retrofit对象

--- library/src/main/java/com/xcheng/retrofit/RetrofitManager.java
+++ library/src/main/java/com/xcheng/retrofit/RetrofitManager.java
@@ -2,6 +2,9 @@ package com.xcheng.retrofit;
 
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import retrofit2.Retrofit;
 
 /**
@@ -12,9 +15,7 @@ import retrofit2.Retrofit;
 public final class RetrofitManager {
     //全局的Retrofit对象，volatile保证可见性，赋值后同步刷新到主内存
     private volatile static Retrofit sRetrofit;
-
-    private RetrofitManager() {
-    }
+    private static final Map<String, Retrofit> sRetrofitMap = new HashMap<>(4);
 
     /**
      * 初始化全局的Retrofit对象,like ARouter#LogisticsCenter,ImageLoader#ImageLoaderConfiguration
@@ -30,21 +31,34 @@ public final class RetrofitManager {
         }
     }
 
+    /**
+     * @return true代表已经被初始化了
+     */
     public static boolean hasInit() {
         return sRetrofit != null;
     }
 
     public static <T> T create(Class<T> service) {
-        if (!hasInit()) {
-            throw new IllegalStateException("RetrofitManager must be init with retrofit before using.");
-        }
-        return sRetrofit.create(service);
+        return retrofit().create(service);
     }
 
     public static Retrofit retrofit() {
-        if (!hasInit()) {
+        if (sRetrofit == null) {
             throw new IllegalStateException("RetrofitManager must be init with retrofit before using.");
         }
         return sRetrofit;
+    }
+
+    public synchronized static void put(String name, Retrofit retrofit) {
+        Utils.checkNotNull(retrofit, "retrofit==null");
+        sRetrofitMap.put(name, retrofit);
+    }
+
+    public synchronized static Retrofit get(String name) {
+        return sRetrofitMap.get(name);
+    }
+
+    public synchronized static void remove(String name) {
+        sRetrofitMap.remove(name);
     }
 }
