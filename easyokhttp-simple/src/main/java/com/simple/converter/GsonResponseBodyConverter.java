0d6b96192c20fbd4aeb11013909bafb2ffd60fec
chengxin <chengx@hlsteel.com>
2018-12-12 10:58:37 +0800

    review

--- easyokhttp-simple/src/main/java/com/simple/converter/GsonResponseBodyConverter.java
+++ easyokhttp-simple/src/main/java/com/simple/converter/GsonResponseBodyConverter.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import com.google.gson.Gson;
 import com.google.gson.internal.$Gson$Types;
 import com.simple.okhttp.Tip;
+import com.xcheng.retrofit.BaseGsonConverter;
 import com.xcheng.retrofit.HttpError;
 
 import org.json.JSONException;
@@ -14,19 +15,17 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 
 import okhttp3.ResponseBody;
-import retrofit2.Converter;
 
 /**
  * 创建时间：2018/4/3
  * 编写人： chengxin
  * 功能描述：json解析相关
  */
-final class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+final class GsonResponseBodyConverter<T> extends BaseGsonConverter<T> {
     private final Gson gson;
-    private final Type type;
 
     GsonResponseBodyConverter(Gson gson, Type type) {
-        this.type = type;
+        super(type, $Gson$Types.getRawType(type));
         this.gson = gson;
     }
 
@@ -42,28 +41,21 @@ final class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
             if (code != 0) {
                 throw new HttpError(msg, tip);
             }
-            Class<?> rawType = $Gson$Types.getRawType(type);
             if (Tip.class == rawType) {
                 return (T) tip;
             }
             Object data = jsonObject.get("data");
-            if (data == JSONObject.NULL) {
-                //in case
+            if (isNullJSON(data)) {
+                throw new HttpError("数据为空", tip);
+            } else if (isEmptyJSON(data)) {
                 throw new HttpError("暂无数据", tip);
             }
-            //如果是String 直接返回
-            if (String.class == rawType) {
-                return (T) data.toString();
+            //data 基础类型 如{"msg": "xxx","code": xxx,"data": xxx}
+            T t = convertBaseType(data, rawType);
+            if (t != null) {
+                return t;
             }
-            //data 为Boolean 如{"msg": "手机号格式错误","code": 0,"data": false}
-            if (Boolean.class == rawType && data instanceof Boolean) {
-                return (T) data;
-            }
-            //data 为Integer  如{"msg": "手机号格式错误","code": 0,"data": 12}
-            if (Integer.class == rawType && data instanceof Integer) {
-                return (T) data;
-            }
-            T t = gson.fromJson(data.toString(), type);
+            t = gson.fromJson(data.toString(), type);
             if (t != null) {
                 //防止线上接口修改导致反序列化失败奔溃
                 return t;
