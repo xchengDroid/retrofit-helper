c30ea7cc0ade630672e803873df8cfa52f659046
chengxin <chengx@hlsteel.com>
2019-04-22 16:39:45 +0800

    review

--- library/src/main/java/com/xcheng/retrofit/Call2.java
+++ library/src/main/java/com/xcheng/retrofit/Call2.java
@@ -16,24 +16,4 @@ public interface Call2<T> extends retrofit2.Call<T> {
 
     @Override
     Call2<T> clone();
-
-    /**
-     * 创建时间：2019/2/27
-     * 编写人： chengxin
-     * 功能描述：请求被取消保存取消的信息
-     */
-    final class Cancel {
-        /**
-         * 是否调用了{@link Call2#cancel()}方法
-         */
-        public final boolean byCall2;
-        //请求被取消的时候 可能抛出的异常
-        @Nullable
-        public final Throwable failure;
-
-        Cancel(boolean byCall2, @Nullable Throwable failure) {
-            this.byCall2 = byCall2;
-            this.failure = failure;
-        }
-    }
 }
