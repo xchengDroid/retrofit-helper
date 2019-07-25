7a076fbd081adf987260ef01fe5c9f48532f5f9d
chengxin <chengx@hlsteel.com>
2019-07-25 14:46:57 +0800

    review doc

--- retrofit-helper/src/main/java/com/xcheng/retrofit/LifecycleEvent.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/LifecycleEvent.java
@@ -4,5 +4,11 @@ import android.arch.lifecycle.Lifecycle;
 import android.support.annotation.Nullable;
 
 public interface LifecycleEvent {
+
+    /**
+     * 当生命周期发生改变时回调
+     *
+     * @param event 当前的生命周期
+     */
     void onEvent(@Nullable Lifecycle.Event event);
 }
