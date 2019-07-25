29a723ac621532f7173609cf74fefe2e843abbfe
chengxin <chengx@hlsteel.com>
2019-07-25 14:03:06 +0800

    review

--- retrofit-helper/src/main/java/com/xcheng/retrofit/LifeCallAdapterFactory.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/LifeCallAdapterFactory.java
@@ -1,6 +1,5 @@
 package com.xcheng.retrofit;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.lang.annotation.Annotation;
@@ -11,19 +10,13 @@ import java.util.concurrent.Executor;
 import retrofit2.Call;
 import retrofit2.CallAdapter;
 import retrofit2.Retrofit;
-import retrofit2.SkipCallbackExecutor;
 
 /**
  * just for android post UI thread
  */
 public final class LifeCallAdapterFactory extends CallAdapter.Factory {
     private static final String RETURN_TYPE = LifeCall.class.getSimpleName();
-    private static final Executor SKIP_EXECUTOR = new Executor() {
-        @Override
-        public void execute(@NonNull Runnable command) {
-            command.run();
-        }
-    };
+
     @Nullable
     private final Executor callbackExecutor;
 
@@ -31,6 +24,9 @@ public final class LifeCallAdapterFactory extends CallAdapter.Factory {
         this.callbackExecutor = callbackExecutor;
     }
 
+    /**
+     * 如果用此构造函数，默认的executor为{@link Retrofit#callbackExecutor()}
+     */
     public static LifeCallAdapterFactory create() {
         return new LifeCallAdapterFactory(null);
     }
@@ -59,14 +55,9 @@ public final class LifeCallAdapterFactory extends CallAdapter.Factory {
                     String.format("%s return type must be parameterized as %s<Foo> or %s<? extends Foo>", RETURN_TYPE, RETURN_TYPE, RETURN_TYPE));
         }
         final Type responseType = getParameterUpperBound(0, (ParameterizedType) returnType);
-        boolean skipCallbackExecutor = Utils.isAnnotationPresent(annotations, SkipCallbackExecutor.class);
-        final Executor executor;
-        if (skipCallbackExecutor) {
-            executor = SKIP_EXECUTOR;
-        } else {
-            executor = callbackExecutor != null ? callbackExecutor : retrofit.callbackExecutor();
-            if (executor == null) throw new AssertionError();
-        }
+
+        final Executor executor = callbackExecutor != null ? callbackExecutor : retrofit.callbackExecutor();
+        if (executor == null) throw new AssertionError();
         return new CallAdapter<Object, LifeCall<?>>() {
             @Override
             public Type responseType() {
