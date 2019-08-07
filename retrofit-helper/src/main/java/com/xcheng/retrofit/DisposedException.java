b498499d0ff093cd87fe238f1a8631955ac2c90e
chengxin <chengx@hlsteel.com>
2019-08-07 09:23:19 +0800

    设置继承IOException

--- retrofit-helper/src/main/java/com/xcheng/retrofit/DisposedException.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/DisposedException.java
@@ -1,11 +1,13 @@
 package com.xcheng.retrofit;
 
+import java.io.IOException;
+
 /**
  * 创建时间：2019-08-06
  * 编写人： chengxin
  * 功能描述：当{@link LifeCall#isDisposed()}返回true时抛出的异常
  */
-public class DisposedException extends RuntimeException {
+public class DisposedException extends IOException {
     private static final long serialVersionUID = 7699927425836702496L;
 
     DisposedException(String message) {
