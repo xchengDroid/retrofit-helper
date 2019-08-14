a1079f47b78d84c29a08b4dd6ee19748c7640ffb
chengxin <chengx@hlsteel.com>
2019-08-14 15:01:39 +0800

    添加 nonnull注解

--- retrofit-helper/src/main/java/com/xcheng/retrofit/RealCall.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/RealCall.java
@@ -1,6 +1,7 @@
 package com.xcheng.retrofit;
 
 import android.arch.lifecycle.Lifecycle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 
@@ -19,6 +20,7 @@ final class RealCall<T> implements Call<T> {
         this.delegate = delegate;
     }
 
+    @NonNull
     @Override
     public T execute() throws Throwable {
         try {
