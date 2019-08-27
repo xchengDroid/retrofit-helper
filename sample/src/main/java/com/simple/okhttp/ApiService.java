a8852b19bdcbffcca21262d22961ce837abfba4c
chengxin <chengx@hlsteel.com>
2019-08-27 16:10:20 +0800

    demo

--- sample/src/main/java/com/simple/okhttp/ApiService.java
+++ sample/src/main/java/com/simple/okhttp/ApiService.java
@@ -2,16 +2,21 @@ package com.simple.okhttp;
 
 import com.simple.entity.Article;
 import com.simple.entity.LoginInfo;
+import com.simple.entity.RegisterInfo;
 import com.simple.entity.WXArticle;
 import com.xcheng.retrofit.Call;
 
 import java.io.File;
 import java.util.List;
 
+import okhttp3.ResponseBody;
+import retrofit2.SkipCallbackExecutor;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Streaming;
 
 /**
  * 创建时间：2018/4/8
@@ -20,9 +25,21 @@ import retrofit2.http.POST;
  */
 public interface ApiService {
     //登录
+    //@SkipCallbackExecutor
+
+    @FormUrlEncoded
+    @POST("user/register")
+    Call<RegisterInfo> register(@Field("username") String username, @Field("password") String password);
+
     @FormUrlEncoded
     @POST("user/login")
     Call<LoginInfo> getLogin(@Field("username") String username, @Field("password") String password);
+
+    //@SkipCallbackExecutor
+    @Streaming
+    @FormUrlEncoded
+    @POST("user/login")
+    retrofit2.Call<ResponseBody> getLoginTestOnMainThread(@Field("username") String username, @Field("password") String password);
 
     //获取微信公众号列表
     @GET("wxarticle/chapters/json")
@@ -37,8 +54,11 @@ public interface ApiService {
     Call<File> loadDouYinApk();
 
     //下载文件
+    // @Streaming
+    @SkipCallbackExecutor
+    @Headers("LogLevel:BASIC")
     @GET("http://shouji.360tpcdn.com/181115/4dc46bd86bef036da927bc59680f514f/com.ss.android.ugc.aweme_330.apk")
-    Call<File> lifeCycleTest();
+    retrofit2.Call<ResponseBody> loadDouYinApkTestOnMainThread();
 }
 
 
