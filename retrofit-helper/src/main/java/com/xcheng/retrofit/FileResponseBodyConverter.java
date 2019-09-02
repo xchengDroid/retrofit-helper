1e402aaca5cf10edab6b88dc23f343c434397439
chengxin <chengx@hlsteel.com>
2019-09-02 14:23:08 +0800

    简化方法

--- retrofit-helper/src/main/java/com/xcheng/retrofit/FileResponseBodyConverter.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/FileResponseBodyConverter.java
@@ -1,9 +1,7 @@
 package com.xcheng.retrofit;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
@@ -20,38 +18,6 @@ public class FileResponseBodyConverter implements Converter<ResponseBody, File> 
 
     @Override
     public File convert(ResponseBody value) throws IOException {
-        File file = new File(path);
-        if (file.exists() && !file.delete()) {
-            throw new IOException("failed to delete file:" + file.getPath());
-        }
-
-        File tmp = new File(file.getPath() + ".tmp");
-
-        if (tmp.exists() && !tmp.delete()) {
-            throw new IOException("failed to delete tmp file:" + tmp.getPath());
-        }
-        InputStream is = value.byteStream();
-        FileOutputStream fos = null;
-        try {
-            if (!tmp.createNewFile()) {
-                throw new IOException("failed to create file:" + tmp.getPath());
-            }
-
-            fos = new FileOutputStream(tmp);
-            byte[] buffer = new byte[8096];
-            int last = 0, c, ct = 0;
-            while ((c = is.read(buffer)) != -1) {
-                ct += c;
-                fos.write(buffer, 0, c);
-            }
-            if (!tmp.renameTo(file)) {
-                throw new IOException("failed to rename file:" + tmp.getPath());
-            }
-            return file;
-        } catch (IOException e) {
-            throw new IOException(e.getMessage());
-        } finally {
-            Utils.close(fos);
-        }
+        return Utils.writeToFile(value, path);
     }
 }
