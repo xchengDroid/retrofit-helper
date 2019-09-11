07f727363ecfc1372d46b377c7dd05f10597ccd8
chengxin <chengx@hlsteel.com>
2019-09-11 11:53:25 +0800

    添加OptionalExecutor

---
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/OptionalExecutor.java
@@ -0,0 +1,27 @@
+package com.xcheng.retrofit;
+
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+
+/**
+ * 创建时间：2019-09-11
+ * 编写人： chengxin
+ * 功能描述：默认的Executor，不做任何操作
+ */
+final class OptionalExecutor implements Executor {
+
+    private static OptionalExecutor EXECUTOR = new OptionalExecutor();
+
+    static OptionalExecutor get() {
+        return EXECUTOR;
+    }
+
+    private OptionalExecutor() {
+    }
+
+    @Override
+    public void execute(@NonNull Runnable command) {
+        command.run();
+    }
+}
