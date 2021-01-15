186451b61e0fcd255f7efcaab29a1d6443da377a
chengxin <chengx@hlsteel.com>
2021-01-15 17:01:39 +0800

    优化OptionalExecutor

--- retrofit-helper/src/main/java/com/xcheng/retrofit/OptionalExecutor.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/OptionalExecutor.java
@@ -1,5 +1,8 @@
 package com.xcheng.retrofit;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import androidx.annotation.NonNull;
 
 import java.util.concurrent.Executor;
@@ -9,12 +12,16 @@ import java.util.concurrent.Executor;
  * 编写人： chengxin
  * 功能描述：默认的Executor，不做任何操作 like retrofit2.Platform
  */
-final class OptionalExecutor implements Executor {
-
+public final class OptionalExecutor implements Executor {
     private static final OptionalExecutor EXECUTOR = new OptionalExecutor();
+    private static final MainThreadExecutor MAIN_THREAD_EXECUTOR = new MainThreadExecutor();
 
-    static OptionalExecutor get() {
+    public static Executor getExecutor() {
         return EXECUTOR;
+    }
+
+    public static Executor getMainThreadExecutor() {
+        return MAIN_THREAD_EXECUTOR;
     }
 
     private OptionalExecutor() {
@@ -23,5 +30,21 @@ final class OptionalExecutor implements Executor {
     @Override
     public void execute(@NonNull Runnable command) {
         command.run();
+    }
+
+    static final class MainThreadExecutor implements Executor {
+        private final Handler handler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable command) {
+            handler.post(command);
+        }
+    }
+
+    /**
+     * 是否为主线程
+     */
+    public static boolean isMainThread() {
+        return Looper.getMainLooper().getThread() == Thread.currentThread();
     }
 }
