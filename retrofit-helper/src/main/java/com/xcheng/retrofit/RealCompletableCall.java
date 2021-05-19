c5d8c293d5ee37d7bc828f0c190cc345c9cb99af
chengxin <chengx@hlsteel.com>
2021-05-19 15:22:17 +0800

    review

--- retrofit-helper/src/main/java/com/xcheng/retrofit/RealCompletableCall.java
+++ retrofit-helper/src/main/java/com/xcheng/retrofit/RealCompletableCall.java
@@ -17,49 +17,7 @@ public final class RealCompletableCall<T> implements CompletableCall<T> {
         this.callbackExecutor = callbackExecutor;
         this.delegate = delegate;
     }
-
-    //@Override
-    //public void onResponse(retrofit2.Call<ResponseBody> call, Response<ResponseBody> response) {
-    //    boolean signalledCallback = false;
-    //    try {
-    //        if (response.body() == null) {
-    //            signalledCallback = true;
-    //            callFailure(new HttpException(response));
-    //            return;
-    //        }
-    //        ResponseBody responseBody = new ProgressResponseBody(response.body()) {
-    //            long lastProgress;
-    //
-    //            @Override
-    //            protected void onDownload(long progress, long contentLength, boolean done) {
-    //                if (pauseProgress)
-    //                    return;
-    //                if (progress - lastProgress > increaseOfProgress * contentLength || done) {
-    //                    lastProgress = progress;
-    //                    callProgress(progress, contentLength, done);
-    //                }
-    //            }
-    //        };
-    //        T body = callback.convert(RealDownloadCall.this, responseBody);
-    //        signalledCallback = true;
-    //        if (body != null) {
-    //            callSuccess(body);
-    //        } else {
-    //            callFailure(new NullPointerException("callback.convert return null"));
-    //        }
-    //    } catch (Throwable t) {
-    //        //如果为OptionalExecutor，不存在线程调度的情况下callXXX方法可能会在当前线程抛出异常
-    //        //会导致callFailure方法调用两次
-    //        //参考okhttp3.RealCall
-    //        //参考 FutureTask#run方法ran标记
-    //        if (!signalledCallback) {
-    //            callFailure(t);
-    //        } else {
-    //            Log.w(RetrofitFactory.TAG, "Callback failure", t);
-    //        }
-    //    }
-    //}
-
+    
     @Override
     public void enqueue(final Callback<T> callback) {
         Objects.requireNonNull(callback, "callback==null");
